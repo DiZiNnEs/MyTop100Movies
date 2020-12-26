7df12d1fbf30c01b3086cb2cae6ca2952c3de2cc
Iglek Kuat <dizinnes228@gmail.com>
2020-12-27 01:35:05 +0600

    refactor: add `user` field in models.py and register models in admin panel

--- film_app/models.py
+++ film_app/models.py
@@ -1,1 +1,12 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Film(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    publication_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user} + {self.title}'
