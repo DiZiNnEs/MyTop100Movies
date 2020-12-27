8029ab9f3e1396aee34a5e28e088978fd3f82c8e
Iglek Kuat <dizinnes228@gmail.com>
2020-12-27 11:08:58 +0600

    feat: connect djoser

--- own_selection_top_films/settings.py
+++ own_selection_top_films/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'djoser',
+
     'film_app.apps.FilmAppConfig',
+
 ]
 
 MIDDLEWARE = [
