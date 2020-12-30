ec6b62792ad8ae68ea9aee24456e626ec8dcf7e7
dizinnes <dizinnes228@gmail.com>
2020-12-30 20:24:36 +0600

    feat: add `user` field

--- film_app/serializers.py
+++ film_app/serializers.py
@@ -4,6 +4,8 @@ from film_app.models import Film
 
 
 class FilmSerializers(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(slug_field='username', read_only=True)
+
     class Meta:
         model = Film
-        fields = ['title', 'description', 'publication_date']
+        fields = ['title', 'description', 'publication_date', 'user']
