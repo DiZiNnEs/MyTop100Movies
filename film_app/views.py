968199edfdfe74d02c9861ee702dfe1ba58870e8
dizinnes <dizinnes228@gmail.com>
2020-12-31 19:23:31 +0600

    refactor: rewrite 'CreateFilmView' to using generics

--- film_app/views.py
+++ film_app/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -12,7 +12,6 @@ from film_app.models import Film
 from film_app.serializers import FilmSerializers, CreateFilmSerializer
 
 
-
 class FilmViews(mixins.ListModelMixin, generics.GenericAPIView):
     queryset = Film.objects.all()
     serializer_class = FilmSerializers
@@ -21,12 +20,9 @@ class FilmViews(mixins.ListModelMixin, generics.GenericAPIView):
         return self.list(request, *args, **kwargs)
 
 
-class CreateFilmView(APIView):
-    def post(self, request):
-        review = CreateFilmSerializer(data=request.data)
-        if review.is_valid():
-            review.save()
-        return Response(status=201)
+class CreateFilmView(CreateAPIView):
+    queryset = Film
+    serializer_class = CreateFilmSerializer
 
 
 class CustomAuthToken(ObtainAuthToken):
