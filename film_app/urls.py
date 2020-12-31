f059238ce955d4c9fabbc49886f58a5f5ba0c69f
dizinnes <dizinnes228@gmail.com>
2020-12-31 19:50:11 +0600

    refactor: delete custom auth token and add him from djoser

--- film_app/urls.py
+++ film_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from film_app.views import FilmViews, CreateFilmView, CustomAuthToken
+from film_app.views import FilmViews, CreateFilmView
 
 urlpatterns = [
     path('films/', FilmViews.as_view()),
     path('create-film/', CreateFilmView.as_view()),
-    path('api-token-auth/', CustomAuthToken.as_view())
 ]
--- own_selection_top_films/urls.py
+++ own_selection_top_films/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('film_app.urls')),
+    path('api/v1/auth/', include('djoser.urls.authtoken')),
 ]
