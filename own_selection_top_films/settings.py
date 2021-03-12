26376dce3fed48bb27ba301cf63e2d34cb99e93e
dizinnes <dizinnes228@gmail.com>
2021-03-13 01:23:47 +0600

    refactor(settings): Update indents

--- own_selection_top_films/settings.py
+++ own_selection_top_films/settings.py
@@ -37,11 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
-
     'film_app.apps.FilmAppConfig',
 
 ]
