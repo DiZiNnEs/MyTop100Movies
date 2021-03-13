1b1f1aaf822d46aba9e86fb0468e9a655430c30b
dizinnes <dizinnes228@gmail.com>
2021-03-13 15:29:08 +0600

    feat(admin): Register models in admin.py

--- film_app/admin.py
+++ film_app/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from film_app.models import Film
+from film_app.models import (
+    Film,
+    Movie,
+    Actor,
+    Genre,
+    Category,
+)
 
 admin.site.register(Film)
+admin.site.register(Movie)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Category)
