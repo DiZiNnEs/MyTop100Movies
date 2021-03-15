22875afe91798352ec178d4ada06a05c2a39e212
dizinnes <dizinnes228@gmail.com>
2021-03-15 13:52:09 +0600

    feat(views): Add detail film view

--- film_app/views.py
+++ film_app/views.py
@@ -1,20 +1,27 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework import generics
 from rest_framework.response import Response
-
 
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-
 
 from film_app.models import Film
 from film_app.serializers import FilmSerializers, CreateFilmSerializer
 
 
-class FilmViews(ListAPIView):
+class FilmViews(generics.ListAPIView):
     queryset = Film.objects.all()
     serializer_class = FilmSerializers
 
 
-class CreateFilmView(CreateAPIView):
+class FilmDetailView(generics.ListAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializers
+
+    def get_queryset(self):
+        q = Film.objects.filter(user=self.request.user)
+        return q
+
+
+class CreateFilmView(generics.CreateAPIView):
     queryset = Film
     serializer_class = CreateFilmSerializer
