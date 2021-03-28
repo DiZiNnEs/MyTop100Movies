a09165d0e295d84219c39bf7eb52faba6ce7b827
dizinnes <dizinnes228@gmail.com>
2021-03-28 17:19:57 +0600

    refactor(api): Add get_queryset

--- film_app/routers.py
+++ film_app/routers.py
@@ -5,7 +5,7 @@ from film_app.views import MovieViewSet
 
 
 router = DefaultRouter()
-router.register(r'movies', MovieViewSet)
+router.register(r'movies', MovieViewSet, basename='Movies')
 
 urlpatterns = [
     path('', include(router.urls)),
--- film_app/views.py
+++ film_app/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -8,11 +7,8 @@ from film_app.serializers import MovieSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-    @action(detail=False, methods=['get'])
-    def movie(self, request):
-        q = self.queryset.filter(user=request.user)
-        result = MovieSerializer(q).data
-        return Response(result, status=status.HTTP_200_OK)
+    def get_queryset(self):
+        user = self.request.user
+        return Movie.objects.filter(user=user)
